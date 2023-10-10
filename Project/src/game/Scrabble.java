a0570572a081d2beeed3f6d7aa0a4ebaa907a6d2
KU3495 <sorrawit.sas@ku.th>
2023-10-10 15:04:26 +0700

    update swap button

--- Project/src/game/Scrabble.java
+++ Project/src/game/Scrabble.java
@@ -6,15 +6,21 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
 
-public class Scrabble extends JFrame{
-	JButton[][] boardButton=new JButton[15][15];
-	JButton[] handButton=new JButton[7];
+public class Scrabble extends JFrame implements MouseListener{
+	public JButton[][] boardButton=new JButton[15][15];
+	public JButton[] handButton=new JButton[7];
 	private int[] handarray=new int[7];
 	private int i=0,j=0;
+	public JPanel MainPanel=new JPanel();
+	public JPanel HandPanel=new JPanel();
 	
 	private int[][] boardarray={
             {4, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 2, 0, 0, 4},
@@ -34,49 +40,62 @@ public class Scrabble extends JFrame{
             {4, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 2, 0, 0, 4}
         };
 	
+	public void Swap() {
+		Random random = new Random();
+		for(i=0; i<7; i++) {
+			char randomLetter = (char) (random.nextInt(26) + 'A');
+			handButton[i]= new JButton(String.valueOf(randomLetter));
+			HandPanel.add(handButton[i]);
+		}
+	}
+	
 	public Scrabble(String title) {
 		super(title);
 		Container MainPane= getContentPane();
 		JLayeredPane LayerPane = getLayeredPane();
-		ImageIcon board=new ImageIcon(this.getClass().getResource("/3W.png"));
-		JPanel MainPanel=new JPanel();
-		MainPanel.setPreferredSize(new Dimension(700,700));
+		ImageIcon icon2w=new ImageIcon(this.getClass().getResource("/2Wnew.png"));
+		ImageIcon icon3w=new ImageIcon(this.getClass().getResource("/3Wnew.png"));
+		ImageIcon icon2L=new ImageIcon(this.getClass().getResource("/2Lnew.png"));
+		ImageIcon icon3L=new ImageIcon(this.getClass().getResource("/3Lnew.png"));
+		
+		MainPanel.setPreferredSize(new Dimension(675,675));
 		MainPanel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 		MainPanel.setLayout(new GridLayout(15,15));
-		MainPanel.setMaximumSize(new Dimension(700, 700));
+		MainPanel.setMaximumSize(new Dimension(675, 675));
 	    MainPanel.setBorder(BorderFactory.createTitledBorder("Test Board"));
-	    getContentPane().add(MainPanel);
+	    //getContentPane().add(MainPanel);
 	    
-		JPanel HandPanel=new JPanel();
+		for(i=0; i<15; i++) {
+			for(j=0; j<15; j++) {
+				boardButton[i][j]= new JButton("");
+				switch(boardarray[i][j]) {
+				case 1: 
+						boardButton[i][j].setIcon(icon2L);
+				break;
+				case 2: 
+						boardButton[i][j].setIcon(icon3L); 
+				break;
+				case 3: 
+						boardButton[i][j].setIcon(icon2w);
+				break;
+				case 4: 
+						boardButton[i][j].setIcon(icon3w); 
+				break;
+				}
+				
+				//boardButton[i][j].setFont(new Font("MS UI Gothic", Font.BOLD, 9));
+				//boardButton[i][j].setForeground(Color.WHITE);
+				boardButton[i][j].setBackground(Color.DARK_GRAY);
+				MainPanel.add(boardButton[i][j]);
+			}
+		}
+		
 		HandPanel.setPreferredSize(new Dimension(600,50));
 		HandPanel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 		HandPanel.setLayout(new GridLayout(1,7));
 		HandPanel.setMaximumSize(new Dimension(600, 50));
-	    HandPanel.setBorder(BorderFactory.createTitledBorder("Test HAND"));
-	    getContentPane().add(HandPanel);
-		
-		for(i=0; i<15; i++) {
-			for(j=0; j<15; j++) {
-				switch(boardarray[i][j]) {
-				case 0: boardButton[i][j]= new JButton("");
-				break;
-				case 1: boardButton[i][j]= new JButton("2L");
-				break;
-				case 2: boardButton[i][j]= new JButton("3L");
-				break;
-				case 3: boardButton[i][j]= new JButton("2W");
-				break;
-				case 4: boardButton[i][j]= new JButton("3W");
-						boardButton[i][j].setIcon(board); 
-				break;
-				}
-				
-				boardButton[i][j].setFont(new Font("MS UI Gothic", Font.BOLD, 9));
-				boardButton[i][j].setForeground(Color.WHITE);
-				boardButton[i][j].setBackground(Color.BLACK);
-				MainPanel.add(boardButton[i][j]);
-			}
-		}
+		HandPanel.setBorder(BorderFactory.createTitledBorder("Test HAND"));
+		//getContentPane().add(HandPanel);
 		
 		Random random = new Random();
 		for(i=0; i<7; i++) {
@@ -84,6 +103,8 @@ public class Scrabble extends JFrame{
 			handButton[i]= new JButton(String.valueOf(randomLetter));
 			HandPanel.add(handButton[i]);
 		}
+		handButton[6]= new JButton("SWAP");
+		HandPanel.add(handButton[6]);
 		
 		JPanel TestPanel=new JPanel();
 		TestPanel.setPreferredSize(new Dimension(600,50));
@@ -91,7 +112,7 @@ public class Scrabble extends JFrame{
 		TestPanel.setLayout(new GridLayout(4,1));
 		TestPanel.setMaximumSize(new Dimension(50, 500));
 	    TestPanel.setBorder(BorderFactory.createTitledBorder("Test option"));
-	    getContentPane().add(TestPanel);
+	    //getContentPane().add(TestPanel);
 		
 		MainPane.add(MainPanel);
 		MainPane.add(HandPanel);
@@ -107,6 +128,35 @@ public class Scrabble extends JFrame{
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setVisible(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	public static void main(String[] args) {
